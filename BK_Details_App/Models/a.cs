d514d3dd74a30ffdb5e2fa6827fb2307c51b241c
user <user@KUZNECOV>
2025-04-02 14:55:55 +0300

    try model

---
+++ BK_Details_App/Models/a.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace BK_Details_App.Models
+{
+    public class a
+    {
+        public class InputData
+        {
+            [LoadColumn(0)] public string Column1 { get; set; }
+            [LoadColumn(1)] public string Column2 { get; set; }
+        }
+
+        public class OutputData
+        {
+            [ColumnName("PredictedLabel")] public string MatchCategory { get; set; }
+        }
+
+        public static void TrainAndSaveModel(string dataPath, string modelPath)
+        {
+            var mlContext = new MLContext();        
+            
+            // Загружаем данные из CSV (файл должен иметь заголовки)
+            IDataView data = mlContext.Data.LoadFromTextFile<InputData>(dataPath, separatorChar: ',', hasHeader: true);        
+            
+            // Определяем пайплайн обработки данных и обучения модели
+            var pipeline = mlContext.Transforms.Conversion.MapValueToKey("Label").Append(mlContext.Transforms.Text.FeaturizeText("Features", "Column1"))            
+                .Append(mlContext.Transforms.Text.FeaturizeText("Features", "Column2"))            
+                .Append(mlContext.MulticlassClassification.Trainers.SdcaMaximumEntropy("Label", "Features"))            
+                .Append(mlContext.Transforms.Conversion.MapKeyToValue("PredictedLabel"));        
+            
+            Console.WriteLine("Обучение модели...");        
+            var model = pipeline.Fit(data);        
+            
+            // Сохраняем модель
+            mlContext.Model.Save(model, data.Schema, modelPath);        
+            Console.WriteLine($"Модель сохранена в {modelPath}");    
+        }    
+        
+        //public static void Main()    
+        //{        
+        //    string dataPath = "data.csv";   
+            
+        //    // Укажи путь к CSV-файлу
+        //    string modelPath = "model.zip"; 
+            
+        //    // Имя файла модели
+        //    TrainAndSaveModel(dataPath, modelPath);   
+        //}
+    }
+    
+    
+    
+}
+
+
---
+++ myapp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+namespace myapp
+{
+    internal class Program
+    {
+        // Класс для входных данных
+        public class InputData
+        {
+            [LoadColumn(0)] public string Column1 { get; set; }
+            [LoadColumn(1)] public string Column2 { get; set; }
+            [LoadColumn(2)] public string Label { get; set; }
+        }
+
+        // Класс для предсказанных значений
+        public class OutputData
+        {
+            [ColumnName("PredictedLabel")] public string MatchCategory { get; set; }
+        }
+
+        static void Main()//Samsung Galaxy A52
+        {
+            string modelPath = "model.zip"; // Файл обученной модели
+            var mlContext = new MLContext();
+
+            // Загружаем модель
+            ITransformer model = mlContext.Model.Load(modelPath, out _);
+            var predictor = mlContext.Model.CreatePredictionEngine<InputData, OutputData>(model);
+
+            Console.WriteLine("Введите первую строку:");
+            string input1 = Console.ReadLine();
+
+            Console.WriteLine("Введите вторую строку:");
+            string input2 = Console.ReadLine();
+
+            // Делаем предсказание
+            var inputData = new InputData { Column1 = input1, Column2 = input2 };
+            var result = predictor.Predict(inputData);
+
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine($"Результат: {result.MatchCategory}");
+        }
+    }
+}
